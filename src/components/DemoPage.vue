a994de009310b6ace173ad889bfe54ea868ca884
venkichinnu <venki.kethavath007@gmail.com>
2024-08-01 16:38:36 +0530

    added demo page

---
+++ src/components/DemoPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <h1>This is sample Demo Page</h1>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,13 @@
 <template>
   <q-page class="flex flex-center">
+    <div style="display: grid;">
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     >
+    <q-btn style="margin: 20px;" to="/demo"> Go To Demo Page </q-btn>
+    </div>
   </q-page>
 </template>
 
