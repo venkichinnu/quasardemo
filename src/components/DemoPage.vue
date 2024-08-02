f55b1ebd7b6d5cdc626513147a66b19453dc356c
venkichinnu <venki.kethavath007@gmail.com>
2024-08-02 15:44:37 +0530

    displaying country cards

--- src/components/DemoPage.vue
+++ src/components/DemoPage.vue
@@ -1,16 +1,54 @@
 <template>
-  <div class="row">
-    <div class="col">
-      <h1>This is sample Demo Page</h1>
-    </div>
-
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" v-for="(item, i) in results" :key="i">
+      <img :src="item.flags.png" />
+      <q-card-section>
+        <div class="text-h6">Country Name: {{ item?.name?.common }}</div>
+        <div class="text-subtitle2">Population: {{ item.population }}</div>
+        <div class="text-subtitle2">
+          Languages:
+          <span v-for="lang of item.languages" :key="lang">
+            {{ `${lang} ` }}
+          </span>
+        </div>
+        <div class="text-subtitle2">
+          Capital:
+          <span v-for="(cap, j) in item.capital" :key="j">
+            {{ `${cap} ` }}
+          </span>
+        </div>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
 <script setup>
+import axios from "axios";
+import { onBeforeMount, ref } from "vue";
 
+const results = ref("");
+
+async function fetchCountry(params) {
+  let endpoint = `https://restcountries.com/v3.1/region/${params}`;
+  await axios
+    .get(endpoint)
+    .then((res) => {
+      if (res.status === 200) {
+        results.value = res.data;
+      }
+    })
+    .catch((err) => {
+      console.error(err.error);
+    });
+}
+
+onBeforeMount(() => {
+  fetchCountry("africa");
+});
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
 </style>
